25546204019767cb9c162dd38b63b2f0c3b411e5
AlanMuniz2772 <jorgeamr2772@gmail.com>
2025-02-24 09:10:33 -0600

    conexion de servidor

--- server.js
+++ server.js
@@ -16,11 +16,40 @@ db.connect(err => {
     console.log("Conectado a MySQL");
 });
 
+
+//GETS
 app.get("/clientes", (req, res) => {
     db.query("SELECT * FROM CLIENTES", (err, results) => {
         if (err) throw err;
         res.json(results);
     });
 });
+
+app.get("/productos", (req, res) => {
+    db.query("SELECT * FROM PRODUCTOS", (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+//POSTS
+app.post("/registrar_cliente", (req, res) => {
+    const { NOMBRE, APATERNO, AMATERNO, COLONIA, CALLE, NUMERO, TELEFONO } = req.body;
+
+    const sql = "INSERT INTO CLIENTES (nombre, apaterno, amaterno, colonia, calle, numero, telefono) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const valores = [NOMBRE, APATERNO, AMATERNO, COLONIA, CALLE, NUMERO, TELEFONO];
+
+    db.query(sql, valores, (err, result) => {
+        if (err) {
+            console.error("Error al insertar cliente:", err);
+            res.status(500).json({ error: "Error al registrar el cliente" });
+            return;
+        }
+        res.json({ mensaje: "Cliente registrado correctamente", id: result.insertId });
+    });
+});
+
+
+
 
 app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
